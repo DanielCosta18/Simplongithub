a8dcb3731c52eebb54ff1548b21874353d6f06d2
DanielCosta18 <daniel.costa.simplon@gmail.com>
2022-02-28 14:56:10 +0100

    gitmasteries

--- getProfil.php
+++ getProfil.php
@@ -1,9 +1,9 @@
 <?php
 
-	$response = @file_get_contents("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/".$_GET['name']."?api_key=RGAPI-ce47c040-0b72-42a2-bc42-e4193d453478");
+    $response = @file_get_contents("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/".$_GET['name']."?RGAPI-f20459c0-0e21-40cf-ba21-586d1160615d");
 
 if ($response === FALSE){
-	$response = '{"id": "none","accountId": "none","puuid": "none","name": "inconnu","profileIconId": 0,"revisionDate": 0, "summonerLevel": 0}';
+    $response = '{"id": "none","accountId": "none","puuid": "none","name": "inconnu","profileIconId": 0,"revisionDate": 0, "summonerLevel": 0}';
 }
 
 echo $response;
