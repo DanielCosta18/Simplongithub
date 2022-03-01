09de6effc22ff94cf447770d3240e6b16797d899
DanielCosta18 <daniel.costa.simplon@gmail.com>
2022-03-01 14:08:31 +0100

    profil

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-$.get("./getProfil.php?name=technet")
-.done( function(data){
-    profil = jQuery.parseJSON(data);
-    $( "#pseudo" ).text(profil.name);
-    $("#icon").attr("src","http://ddragon.leagueoflegends.com/cdn/12.4.1/img/profileicon/"+profil.profileIconId+".png")
+pseudo="technet";
 
-});
+
+function profil(){
+    $.get("./getProfil.php?name="+pseudo).done( function(data){
+        profil = jQuery.parseJSON(data);
+        $( "#pseudo" ).text(profil.name);
+        console.log(profil);
+        $("#icon").attr("src","http://ddragon.leagueoflegends.com/cdn/12.4.1/img/profileicon/"+profil.profileIconId+".png")
+    
+    });
+};
+
 $("#enter").click(function(){
     pseudo=$("#joueur").val();
     console.log("pseudo : "+pseudo);
+    profil();
 });
